262d8b7cccad9060a5a3e6427f5455bd124a0587
Dban <dban7171@gmail.com>
2021-05-15 18:19:19 +0900

    시트 이름 설정 및 저장

--- 1_Excel.py
+++ 1_Excel.py
@@ -1,3 +1,9 @@
 from openpyxl import Workbook
 
-wb - Workbook() #새 워크북 생성
+wb = Workbook() #새 워크북 생성
+
+ws = wb.active # 현재 활성화된 sheet 가져온다. 
+
+ws.title = "SJSheet" #sheet의 이름을 변경 
+wb.save("sample.xlsx")
+wb.close()
