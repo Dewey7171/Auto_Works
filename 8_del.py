a9e20586f3e33cfdef806caad8d9c1c61f9b60bb
Dban <dban7171@gmail.com>
2021-05-21 09:34:24 +0900

    파일 저장위치 변경 및 셀 이동 및 차트 생성하기

--- 8_del.py
+++ 8_del.py
@@ -1,10 +1,12 @@
 from openpyxl import load_workbook
 
+
 wb = load_workbook("sample.xlsx")
 ws = wb.active
 
-ws.delete_cols(2) #8번째 줄에 있는 데이터 삭제 
+#ws.delete_cols(2) #8번째 줄에 있는 데이터 삭제 
 
-ws.delete_cols
+#ws.delete_rows(3) # 8 번째 줄에 있는 7번 ㄷ학생 데이터 삭제
 
-wb.save("sample_del_cols.xlsx")
+ws.delete_cols(2,2)
+wb.save("sample_del_cols1.xlsx")
